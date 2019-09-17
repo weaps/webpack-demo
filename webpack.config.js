0439f2b9d80ab038de6f8674e3d7bab1c714f430
weaps <weaps@qq.com>
2019-09-17 23:46:13 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,98 +1,58 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: "[name].main.[hash].js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],  // 用于解析es6语法
-            plugins: ['@babel/plugin-proposal-class-properties'] // 用于解析es7语法
-          }
-        },
-        include: path.resolve(__dirname, 'src'),
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(css|sass|scss)$/,
-        use: [
-          // {
-          //   loader: 'style-loader',
-          //   options: {
-          //     insert: 'top'
-          //   }
-          // },
-          // 如果使用了'mini-css-extract-plugin'插件，就不需要引入style-loader
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
+// npm i purgecss-webpack-plugin glob -D 删除无用的css样式插件
+// const glob = require('glob')
+// glob.sync('./src/**/*') //src下任意文件
+// image-webpack-loader 压缩图片loader
+// webpack-bundle-analyzer.
+module.exports = (env) => {
+  console.log(env, 'xx')
+  return {
+    mode: env,
+    entry: './src/index.js',
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, 'dist')
+    },
+    // externals: {
+    //   'jquery': '$'
+    // },
+    module: {
+      rules: [
+        {
+          test: /\.(css|sass|scss)$/,
+          use: [
+            {
+              loader: "style-loader"
+            },
+              {
+              loader: "css-loader"
+            },
+            {
+              loader: "postcss-loader"
+            },
+            {
+              loader: "sass-loader"
             }
-          },
-          {
-            loader: 'postcss-loader',
-            // options: {
-            //   plugins: [
-            //     require('autoprefixer')({
-            //       browsers: ['last 2 versions']
-            //     })
-            //   ]
-            // }
-          },{
-            loader: 'sass-loader',
-          }
-        ]
-      },
-      {
-        test: /\.(png|gif|jpe?g|svg)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            /*
-              * [ext] 表示原图是什么格式就打包成什么格式
-              * images 表示解析后的图片目录
-            */
-            name: 'images/[name].[hash].[ext]',
-            limit: 100000
-          }
-        }]
-      },
-      // 加载字体
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'font'
-          }
-        }]
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      inject: true,
-      minify: {},
-      hash: true
-    }),
-    // 将CSS提取为独立的文件的插件，对每个包含css的js文件都会创建一个CSS文件，支持按需加载css和sourceMap
-    new MiniCssExtractPlugin({
-      filename: 'main.css' // 设置CSS提取为独立的文件名称
-    })
-  ]
+          ]
+        },
+        {
+          test: /\.(png|gif|jpeg|jpg|svg)$/,
+          use: [
+            {
+              loader: "file-loader"
+            }
+          ]
+        },{
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'font'
+            }
+          }]
+        }
+      ]
+    }
+  }
 }
